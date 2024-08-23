9f6e231e073019675729d681678182c1c1375843
Tejashree <tejashree@strative.ai>
2024-08-22 22:40:44 -0700

    install chrome and delete chrome .deb

--- setup.sh
+++ setup.sh
@@ -8,4 +8,9 @@ git clone https://github.com/flutter/flutter.git /usr/local/flutter
 
 export PATH="/usr/local/flutter/bin:/usr/local/flutter/bin/cache/dart-sdk/bin:${PATH}"
 
+apt update -y
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+apt install ./google-chrome-stable_current_amd64.deb -y
+sudo apt-get clean
+
 flutter doctor -v
